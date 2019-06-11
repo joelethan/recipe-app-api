a7e38fd725f387c52394238f66ee570048ac0441
Katusiime Joel Ian <joelethan@users.noreply.github.com>
2019-06-11 22:58:36 +0300

    Add list recipe feature

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,4 +1,4 @@
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
@@ -60,3 +60,14 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe string representation"""
+        recipe = Recipe.objects.create(
+            user=sample_user(),
+            title='Steak and mushroom sauce',
+            time_minutes=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
